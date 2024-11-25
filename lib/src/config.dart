b39092bc34c5ddee879dc408ae58a7e550da6f60
Mahershi Bhavsar <mahershi1999@gmail.com>
2024-11-25 22:58:26 +0530

    config file

---
+++ lib/src/config.dart
@@ -0,0 +1,57 @@
+// TODO: why is this gitignored
+
+enum Environment {DEVELOPMENT, STAGING}
+
+class Config{
+  static String _host = "http://localhost:8000/";
+  static String _wsHost = "ws://localhost:8000/";
+  static int _port = 8000;
+  static Environment _currentEnvironment = Environment.DEVELOPMENT;
+
+  static const String _tokenURL = 'api/token/';
+  static const String _clientTokenURL = 'api/client-token/';
+  static const String _tokenRefreshURL = 'api/token/refresh/';
+  static const String _userBucketURL = 'app/user-buckets/bucket';
+  static const String _webSocketURL = 'bucket/stream/';
+  static const String _getClient = 'app/project-client/me';
+
+  static const String _userURL = 'app/user/me';
+
+  static Environment get currentEnvironment => _currentEnvironment;
+  static String get host => _host;
+  static String get wsHost => _wsHost;
+  static int get port => _port;
+  static String get tokenURL => _tokenURL;
+  static String get clientTokenURL => _clientTokenURL;
+  static String get tokenRefreshURL => _tokenRefreshURL;
+  static String get userBucketURL => _userBucketURL;
+  static String get webSocketURL => _webSocketURL;
+  static String get userURL => _userURL;
+  static String get getClient => _getClient;
+
+
+
+  static const Map<String, int> typeMap = {
+    "STRING": 1,
+    "NUMBER": 2,
+    "BOOLEAN": 3,
+    "ARRAY": 4,
+    "MAP": 5
+  };
+
+  static void setEnvironment(Environment env){
+    switch(env){
+      case Environment.DEVELOPMENT:
+        // this is default.
+        Config._currentEnvironment = env;
+        print("ENVIRONMENT: DEVELOPMENT");
+        break;
+      case Environment.STAGING:
+        Config._host = "http://192.168.0.200:9999/";
+        Config._wsHost = "ws://192.168.0.200:9999/";
+        Config._port = 9999;
+        Config._currentEnvironment = env;
+        print("ENVIRONMENT: STAGING");
+    }
+  }
+}
